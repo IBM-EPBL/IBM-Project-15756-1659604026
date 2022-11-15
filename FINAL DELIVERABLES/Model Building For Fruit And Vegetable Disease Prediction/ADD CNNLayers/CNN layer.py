f758311fe16ee401068e85905f4ae58cfdbae056
Tamildutt <113963490+Tamildutt@users.noreply.github.com>
2022-11-15 23:56:35 +0530

    Add files via upload

---
+++ FINAL DELIVERABLES/Model Building For Fruit And Vegetable Disease Prediction/ADD CNNLayers/CNN layer.py
@@ -0,0 +1,20 @@
+model = tf.keras.models.Sequential([
+    # Note the input shape is the desired size of the image 200x 200 with 3 bytes color
+    # The first convolution
+    tf.keras.layers.Conv2D(16, (3,3), activation='relu', input_shape=(200, 200, 3)),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    # The second convolution
+    tf.keras.layers.Conv2D(32, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    # The third convolution
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    # The fourth convolution
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    # The fifth convolution
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    # Flatten the results to feed into a dense layer
+    tf.keras.layers.Flatten(),
+])
