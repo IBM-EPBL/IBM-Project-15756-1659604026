df534fc2c76a6a8ccc07cb35a8513188e5c63987
Pradeepn2001 <113963966+Pradeepn2001@users.noreply.github.com>
2022-11-15 23:05:24 +0530

    Add files via upload

---
+++ FINAL DELIVERABLES/Model Building For Fruit And Vegetable Disease Prediction/Add Dense Layers/Dense_layer.py
@@ -0,0 +1,6 @@
+model = tf.keras.models.Sequential([
+    # 128 neuron in the fully-connected layer
+    tf.keras.layers.Dense(128, activation='relu'),
+    # 5 output neurons for 5 classes with the softmax activation
+    tf.keras.layers.Dense(15, activation='softmax')
+])
